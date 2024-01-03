afe0c009e0c6bad0c4b666ee8b0651944ddedb69
Ali T <a.ali.talebi@gmail.com>
2024-01-03 13:59:21 +0330

    Add AspNetIdentity

---
+++ GreenHouse.DomainEntitty/Identity/ApplicationRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GreenHouse.DomainEntitty.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public string? TitleMultilang { get; set; }
+
+        public ApplicationRole() : base()
+        {
+
+        }
+
+        public ApplicationRole(string name) : base(name)
+        {
+
+        }
+
+    }
+
+}
---
+++ GreenHouse.DomainEntitty/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenHouse.DomainEntitty.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessageResourceName = null, ErrorMessageResourceType = null, MinimumLength = 10)]
+        public string NationalCodeId { get; set; }
+
+        public bool IsActive { get; set; }
+        //public ICollection<ProfilePictureUserEntity>? profilePictureUserEntities { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
