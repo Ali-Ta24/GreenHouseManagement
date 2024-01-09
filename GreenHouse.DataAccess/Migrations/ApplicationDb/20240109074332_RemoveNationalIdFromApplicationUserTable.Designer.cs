6bf9eb651fc3dae844f504b46802ded3377ca411
Ali T <a.ali.talebi@gmail.com>
2024-01-09 11:36:07 +0330

    remove NationalId from aspnetuser table

--- GreenHouse.DataAccess/Migrations/ApplicationDb/20240109074332_RemoveNationalIdFromApplicationUserTable.Designer.cs
+++ GreenHouse.DataAccess/Migrations/ApplicationDb/20240109074332_RemoveNationalIdFromApplicationUserTable.Designer.cs
@@ -4,6 +4,7 @@ using GreenHouse.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GreenHouse.DataAccess.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240109074332_RemoveNationalIdFromApplicationUserTable")]
+    partial class RemoveNationalIdFromApplicationUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GreenHouse.DataAccess/Migrations/ApplicationDb/20240109074332_RemoveNationalIdFromApplicationUserTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenHouse.DataAccess.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class RemoveNationalIdFromApplicationUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NationalCodeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NationalCodeId",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
