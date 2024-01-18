9651a6d084b85d881e447dec3ce257d4fe9ff6c1
AliT <a.ali.talebi@gmail.com>
2024-01-18 17:24:32 +0330

    chenge folder name

---
+++ GreenHouse.Web/Controller/MVC/SensorManagement/SensorManagementController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHouse.Web.Controller.MVC.SensorManagement
+{
+    [Authorize]
+    public class SensorManagementController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet]
+        public IActionResult TemperatureSensor()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult HumiditySensor()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LightIntensitySensor()
+        {
+            return View();
+        }
+    }
+}
