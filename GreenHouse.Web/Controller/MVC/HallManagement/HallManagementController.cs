aa5f4aac2037b833c4a659d902df3e6f63084511
Ali T <a.ali.talebi@gmail.com>
2024-01-21 18:04:27 +0330

    add page and cotroller for GreenHouseHall

---
+++ GreenHouse.Web/Controller/MVC/HallManagement/HallManagementController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHouse.Web.Controller.MVC.HallManagement
+{
+    [Authorize]
+    public class HallManagementController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet]
+        public IActionResult GreenHouseHall()
+        {
+            return View();
+        }
+    }
+}
