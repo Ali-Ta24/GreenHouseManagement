a94070350d810fbfb204e5072e2a1ee0df010393
Ali T <a.ali.talebi@gmail.com>
2024-01-24 11:34:46 +0330

    update sensor view

---
+++ GreenHouse.DataAccess/Migrations/20240124074627_UpdateVw_HumiditySensortolastEdition.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenHouse.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVw_HumiditySensortolastEdition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"SET QUOTED_IDENTIFIER ON;
+                                   SET ANSI_NULLS ON;
+                                   GO
+                                   ALTER VIEW Vw_HumiditySensor
+                                   AS
+                                   SELECT *
+                                   FROM
+                                   (
+                                       SELECT dbo.HumiditySensor.ID,
+                                              dbo.HumiditySensor.CreatedBy,
+                                              dbo.HumiditySensor.LastModifiedBy,
+                                              dbo.HumiditySensor.CreationTime,
+                                              dbo.HumiditySensor.LastModificationTime,
+                                              dbo.HumiditySensor.HumiditySensorName,
+                                              dbo.UserGreenhouseHall.HallName,
+                                              dbo.UserGreenhouseHall.ID AS GreenhouseHallID,
+                                              dbo.AspNetUsers.UserName,
+                                              dbo.AspNetUsers.FirstName + N' ' + dbo.AspNetUsers.LastName AS FullName,
+                                              dbo.HumiditySensorDetail.HumiditySensorValue,
+                                              dbo.HumiditySensorDetail.LastModificationTime AS LastState,
+                                              ROW_NUMBER() OVER (PARTITION BY dbo.HumiditySensor.ID
+                                                                 ORDER BY dbo.HumiditySensorDetail.LastModificationTime DESC
+                                                                ) AS row_num
+                                       FROM dbo.HumiditySensor
+                                           INNER JOIN dbo.UserGreenhouseHall
+                                               ON dbo.HumiditySensor.GreenhouseHallID = dbo.UserGreenhouseHall.ID
+                                           LEFT JOIN dbo.AspNetUsers
+                                               ON AspNetUsers.Id = UserGreenhouseHall.UserID
+                                           LEFT JOIN dbo.HumiditySensorDetail
+                                               ON HumiditySensorDetail.HumiditySensorID = HumiditySensor.ID
+                                   ) RankedData
+                                   WHERE RankedData.row_num = 1;
+                                   GO
+                                   
+                                   ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ GreenHouse.DataAccess/Migrations/20240124075108_UpdateVw_LightIntensitySensorToLastEdition.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenHouse.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVw_LightIntensitySensorToLastEdition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"SET QUOTED_IDENTIFIER ON;
+                                   SET ANSI_NULLS ON;
+                                   GO
+                                   ALTER VIEW Vw_LightIntensitySensor
+                                   AS
+                                   SELECT *
+                                   FROM
+                                   (
+                                       SELECT dbo.LightIntensitySensor.ID,
+                                              dbo.LightIntensitySensor.CreatedBy,
+                                              dbo.LightIntensitySensor.LastModifiedBy,
+                                              dbo.LightIntensitySensor.CreationTime,
+                                              dbo.LightIntensitySensor.LastModificationTime,
+                                              dbo.LightIntensitySensor.LightIntensitySensorName,
+                                              dbo.UserGreenhouseHall.HallName,
+                                              dbo.UserGreenhouseHall.ID AS GreenhouseHallID,
+                                              dbo.AspNetUsers.UserName,
+                                              dbo.AspNetUsers.FirstName + N' ' + dbo.AspNetUsers.LastName AS FullName,
+                                              dbo.LightIntensitySensorDetail.LightIntensitySensorValue,
+                                              dbo.LightIntensitySensorDetail.LastModificationTime AS LastState,
+                                              ROW_NUMBER() OVER (PARTITION BY dbo.LightIntensitySensor.ID
+                                                                 ORDER BY dbo.LightIntensitySensorDetail.LastModificationTime DESC
+                                                                ) AS row_num
+                                       FROM dbo.LightIntensitySensor
+                                           INNER JOIN dbo.UserGreenhouseHall
+                                               ON dbo.LightIntensitySensor.GreenhouseHallID = dbo.UserGreenhouseHall.ID
+                                           LEFT JOIN dbo.AspNetUsers
+                                               ON AspNetUsers.Id = UserGreenhouseHall.UserID
+                                           LEFT JOIN dbo.LightIntensitySensorDetail
+                                               ON LightIntensitySensorDetail.LightIntensitySensorID = LightIntensitySensor.ID
+                                   ) RankedData
+                                   WHERE RankedData.row_num = 1;
+                                   GO
+                                   
+                                   ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
